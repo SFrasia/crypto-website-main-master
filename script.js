02743217db1de462484a6fd70e302057b040d469
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-02-06 21:03:26 +0300

    Changes

--- script.js
+++ script.js
@@ -3,47 +3,111 @@
 const searchBtn = document.getElementById("search");
 const table = document.querySelector(".crypto-table");
 const tbody = table.getElementsByTagName("tbody")[0];
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const form = document.querySelector("form");
+const searchInput = document.querySelector("#search_input");
 
-const loadData = async () => {
-  tbody.innerHTML = "";
+async function fetchCrypto() {
   const getData = await fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false"
   );
   const jsonData = await getData.json();
+  return jsonData;
+}
+
+const loadData = async () => {
+  const jsonData = await fetchCrypto();
   const splittedData = jsonData.slice(0, 10);
   showData(splittedData);
-
-  searchBtn.addEventListener("click", (e) => {
-    const searchVal = searchInput.value;
-    if (searchVal === "") {
-      alert("plese write something");
-      return false;
-    } else {
-      searchFun(searchVal, jsonData);
-    }
-  });
 };
 
 function showData(data) {
+  tbody.innerHTML = "";
   let html = "";
-  data.forEach((coin) => {
+  if (data.constructor === Array) {
+    data.forEach((coin) => {
+      const {
+        name,
+        current_price,
+        market_cap_change_percentage_24h: market_cap,
+        price_change_percentage_24h: price_cap,
+      } = coin;
+      html = html += `
+            <tr>
+            <td>${name}</td>
+            <td>$${current_price}</td>
+            <td class= "${
+              +market_cap > 0 ? "success" : "danger"
+            }">${market_cap.toFixed(2)}</td>
+            <td class = "${
+              +price_cap > 0 ? "success" : "danger"
+            }">${price_cap.toFixed(2)}</td>
+            <td><button class="buy show-modal">Buy</button></td>
+            </tr>
+            `;
+    });
+  } else {
     const {
       name,
       current_price,
-      market_cap_change_percentage_24h,
-      price_change_percentage_24h,
-    } = coin;
+      market_cap_change_percentage_24h: market_cap,
+      price_change_percentage_24h: price_cap,
+    } = data;
     html = html += `
-        <tr>
-        <td>${name}</td>
-        <td>${current_price}</td>
-        <td>${market_cap_change_percentage_24h}</td>
-        <td>${price_change_percentage_24h}</td>
-        <td><button class="buy">Buy</button></td>
-        </tr>
-        `;
-  });
+            <tr>
+            <td>${name}</td>
+            <td>$${current_price}</td>
+            <td class= "${
+              +market_cap > 0 ? "success" : "danger"
+            }">${market_cap.toFixed(2)}</td>
+            <td class = "${
+              +price_cap > 0 ? "success" : "danger"
+            }">${price_cap.toFixed(2)}</td>
+            <td><button class="buy show-modal">Buy</button></td>
+            </tr>
+            `;
+  }
+
   tbody.insertAdjacentHTML("afterend", html);
 }
 
 loadData();
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("show-modal")) {
+    openModal();
+  }
+});
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchVal = searchInput.value;
+  if (searchVal === "") {
+    alert("please write something");
+    return false;
+  }
+  const jsonData = await fetchCrypto();
+  const coin = jsonData.find(
+    (cn) => cn.name.toLowerCase() === searchVal.toLowerCase()
+  );
+  showData(coin);
+});
